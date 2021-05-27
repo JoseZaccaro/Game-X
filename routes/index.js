6cc822cd4c030dd312ab94a0b392b1b6a5d17820
Adriel Baez <baez.adriel27@gmail.com>
2021-05-27 19:53:24 -0300

    agregando hardware controllers y rutas

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 const express =require ('express')
 const router = express.Router()
 const passport = require('passport')
+const hardwareControllers = require('../controllers/hardwareControllers')
 
+const{getAllHardwares, getOneHardware, deleteHardware, addNewHardware, updateHardware}=hardwareControllers
 
+// ------------ROUTES HARDWARE---------
+router.route('/hardware')
+.get(getAllHardwares)
+.post(addNewHardware)
+
+router.route('/hardware/:id')
+.get(getOneHardware)
+.delete(deleteHardware)
+.put(updateHardware)
 
 module.exports = router
