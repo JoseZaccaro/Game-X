22a675edf9d1a45a0fdc6af1d4845787d248e104
r3aper79 <nicolasjavier768@hotmail.com>
2021-05-30 19:51:41 -0300

    modificado footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,3 +1,8 @@
+import {Link} from 'react-router-dom'
+
+function openTab(url) {
+    window.open(url)
+}
 const Footer = () =>{ 
     return(
         <div className='containFooter'>
@@ -5,10 +10,10 @@ const Footer = () =>{
             <div className='containNavFooter'>
                 <div className='divContainNavFooter'>
                     <h2 className='nameLogoFooter'>Game-X</h2>
-                    <p className='pContainNavFooter'>I'm a paragraph. Click here to add your own</p>
-                    <p className='pContainNavFooter'>text and edit me. I’m a great place for you to</p>
-                    <p className='pContainNavFooter'>tell a story and let your users know a little</p>
-                    <p className='pContainNavFooter'>more about you.</p>
+                    <p className='pContainNavFooter'>Game-X the first virtual videoGames Company</p>
+                    <p className='pContainNavFooter'>You can find a lot of games here!</p>
+                    <p className='pContainNavFooter'>If you are a gamer, you know our pages!</p>
+                    <p className='pContainNavFooter'>Be a gamer, be a member of our family</p>
                 </div>
                 <div className='divContainNavFooter'>
                     <h3 className='titleSeccionFooter'>Contact</h3>
@@ -19,17 +24,17 @@ const Footer = () =>{
                 </div>
                 <div className='divContainNavFooter'>
                     <h3 className='titleSeccionFooter'>Quick Menu</h3>
-                    <p className='pContainNavFooter'>Games</p>
-                    <p className='pContainNavFooter'>Company</p>
-                    <p className='pContainNavFooter'>Categories</p>
-                    <p className='pContainNavFooter'>Contact</p>
+                    <Link to="/games" className='pContainNavFooter'>Games</Link>
+                    <Link to="/company" className='pContainNavFooter'>Company</Link>
+                    <Link to="/categories" className='pContainNavFooter'>Categories</Link>
+                    <Link to="/contact" className='pContainNavFooter'>Contact</Link>
                 </div>
                 <div className='divContainNavFooter'>
                     <h3 className='titleSeccionFooter'>Socials</h3>
-                    <p className='pContainNavFooter'>Facebook</p>
-                    <p className='pContainNavFooter'>Instagram</p>
-                    <p className='pContainNavFooter'>Twitter</p>
-                    <p className='pContainNavFooter'>Youtube</p>
+                   <a target="_blank" href="http://facebook.com" className='pContainNavFooter'>Facebook</a>
+                   <a target="_blank" href="http://instagram.com" className='pContainNavFooter'>Instagram</a>
+                   <a target="_blank" href="http://twitter.com" className='pContainNavFooter'>Twitter</a>
+                   <a target="_blank" href="http://youtube.com" className='pContainNavFooter'>Youtube</a>
                 </div>
             </div>
         </div>       
