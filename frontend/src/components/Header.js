27f936b55a18d39e6fa4da5a394fede1564ecdac
matias <matidaless@gmail.com>
2021-05-30 18:21:39 -0300

    backup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,6 @@ import userActions from '../redux/actions/userActions'
 
 const Header = (props) =>{ 
 
-
     const logOut=(()=> {
         swal({
             title: "Are you sure that you want to Log Out?",
@@ -14,15 +13,15 @@ const Header = (props) =>{
             icon: "warning",
             buttons: ["No Way!", "I'm Sure!"],
             dangerMode: true,
-          })
-          .then((willDelete) => {
+        })
+        .then((willDelete) => {
             if (willDelete) {
                 props.removeUserInfo()
                 swal("Okay then, see you later!", {
                     icon: "success",
-              });
+                });
             } 
-          });
+        });
     })
     
     let image = ""
@@ -30,31 +29,36 @@ const Header = (props) =>{
     if ( props.userLogged  && props.userLogged.imageUrl) {
         image = props.userLogged.imageUrl
     } else if ( props.userLogged && !props.userLogged.imageUrl && props.userLogged.avatar) {
-        image = "/client/build/assets/60b2c7b6723fdb1630b75246.jpg"        
+        image = props.userLogged.avatar      
     } else {
         image = "assets/generic-user-icon.jpg"
     }
     
-
+    
     return(
         <div className='containHeader'>
             <div className='logoHeader'>
                 <div className='gifLogo' style={{backgroundImage:'url("../assets/logoGif.gif")'}}></div>
                 <h1 className='nameLogoHeader'>Game-X</h1>
             </div>
-            <div className='navHeader'>
-                <NavLink to='/'><p className='linkHeaderNav'>Home</p></NavLink>
-                <NavLink to='/store'><p className='linkHeaderNav'>Store</p></NavLink>
-                {props.userLogged && props.userLogged.rol === "admin" && <NavLink to='/admin'><p className='linkHeaderNav'>Adm Panel</p></NavLink>}
-                {!props.userLogged && <NavLink to='/access'><p className='linkHeaderNav'>Access</p></NavLink>}
-            </div>
-            <div className='profileBody'>
-                <div className="nav">
-                    <input type="checkbox"/>
-                    <div style={{backgroundImage: `url("${image}")`}} className="avatarHeader" ></div>
+            <div className='navContainer'>
+                <div className='navHeader'>
+                    <NavLink to='/'><p className='linkHeaderNav'>Home</p></NavLink>
+                    <NavLink to='/store'><p className='linkHeaderNav'>Store</p></NavLink>
+                    {props.userLogged && props.userLogged.rol === "admin" && <NavLink to='/admin'><p className='linkHeaderNav'>Adm Panel</p></NavLink>}
+                    {!props.userLogged && <NavLink to='/access'><p className='linkHeaderNav'>Access</p></NavLink>}
+                </div>
+                <div className='profileBody'>
+                    <div className="nav">
+                        <input type="checkbox"/>
+                        <div style={{backgroundImage: `url("${image}")`}} className="avatarHeader" />
                         <div className="menu">
+                            <li >Favorites</li>
+                            <li >My Buys</li>
+                            <li >Chat</li>
                             <li onClick={(e)=>logOut(e.target)}>LogOut</li>
                         </div>
+                    </div>
                 </div>
             </div>
         </div>       
