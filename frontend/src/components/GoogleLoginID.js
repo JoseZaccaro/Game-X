1dc4011f1a89f51fe1ab5ae4b8c06700d7897302
matias dalessio <matidaless@gmail.com>
2021-06-08 17:18:55 -0300

    changes

--- frontend/src/components/GoogleLoginID.js
+++ frontend/src/components/GoogleLoginID.js
@@ -1,2 +1,3 @@
 // export const GoogleLoginID = '768453080794-eniki1rdd58l10r33l9ceu5bso04ak6o.apps.googleusercontent.com'
 export const GoogleLoginID = '768453080794-pkujsv6b37tgu62biku1bt4f3pjj2658.apps.googleusercontent.com'
+
